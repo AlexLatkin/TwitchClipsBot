c8dae874ada448b457cea5e392db497a247bc046
Alexey <alexlatkin3@gmail.com>
2024-02-17 21:18:21 +0300

    Nvm

--- src/main/java/com/alexlatkin/twitchclipsbot/service/VideoClipsService.java
+++ src/main/java/com/alexlatkin/twitchclipsbot/service/VideoClipsService.java
@@ -1,10 +1,15 @@
 package com.alexlatkin.twitchclipsbot.service;
 
+import com.alexlatkin.twitchclipsbot.dto.TwitchUser;
 import com.alexlatkin.twitchclipsbot.dto.VideoClipsDto;
 
+import java.io.IOException;
+import java.net.URISyntaxException;
 import java.util.List;
 
 public interface VideoClipsService {
     VideoClipsDto getVideoClipsByBroadcastersId(List<Long> BroadcasterId);
     VideoClipsDto getVideoClipsByGameId(Long gameId);
+    Long getGames(String gameName);
+    TwitchUser getUsers(String broadcasterName) throws IOException, InterruptedException, URISyntaxException;
 }

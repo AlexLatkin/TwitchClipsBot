3a1bc7bf59e47f5b18c0e3f62e233969fba43e4b
AlexLatkin <alexlatkin3@gmail.com>
2024-03-10 22:35:43 +0300

    update

--- src/main/java/com/alexlatkin/twitchclipsbot/model/entity/User.java
+++ src/main/java/com/alexlatkin/twitchclipsbot/model/entity/User.java
@@ -1,29 +1,30 @@
 package com.alexlatkin.twitchclipsbot.model.entity;
 
 
-//import jakarta.persistence.*;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
 import lombok.Data;
-import java.util.List;
 
-//@Data
-//@Entity(name = "user")
-//public class User {
-//    @Id
-//    @Column(name = "chat_id")
-//    private Long chatId;
-//    @Column(name = "name")
-//    private String userName;
-//
+@Data
+@Entity(name = "users")
+public class User {
+    @Id
+    @Column(name = "chat_id")
+    private Long chatId;
+    @Column(name = "name")
+    private String userName;
+
 //    @ManyToMany(cascade = CascadeType.ALL)
-//    @JoinTable(name = "user_favorite_list"
-//            , joinColumns = @JoinColumn(name = "user_id")
+//    @JoinTable(name = "users_favorite_list"
+//            , joinColumns = @JoinColumn(name = "users_id")
 //            , inverseJoinColumns = @JoinColumn(name = "favorite_list_id"))
 //    private List<FavoriteList> favoriteList;
 //
 //    @ManyToMany
-//    @JoinTable(name = "user_black_list"
-//            , joinColumns = @JoinColumn(name = "user_id")
+//    @JoinTable(name = "users_black_list"
+//            , joinColumns = @JoinColumn(name = "users_id")
 //            , inverseJoinColumns = @JoinColumn(name = "black_list_id"))
 //    private List<BlackList> blackList;
-//
-//}
+
+}
--- src/main/java/com/alexlatkin/twitchclipsbot/model/repository/BlackListRepository.java
+++ src/main/java/com/alexlatkin/twitchclipsbot/model/repository/BlackListRepository.java
@@ -2,6 +2,6 @@ package com.alexlatkin.twitchclipsbot.model.repository;
 
 //import com.alexlatkin.twitchclipsbot.model.entity.BlackList;
 //import org.springframework.data.jpa.repository.JpaRepository;
-
+//
 //public interface BlackListRepository extends JpaRepository<BlackList, Integer> {
 //}

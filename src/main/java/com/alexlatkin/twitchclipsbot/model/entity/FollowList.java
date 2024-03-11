53785db4107f65e511bb362753dea68db5323b31
AlexLatkin <alexlatkin3@gmail.com>
2024-03-11 20:55:32 +0300

    update

--- src/main/java/com/alexlatkin/twitchclipsbot/model/entity/FollowList.java
+++ src/main/java/com/alexlatkin/twitchclipsbot/model/entity/FollowList.java
@@ -6,17 +6,16 @@ import lombok.Data;
 import java.util.List;
 
 @Data
-@Entity(name = "black_list")
-public class BlackList {
+@Entity(name = "follow_list")
+public class FollowList {
     @Id
     @Column(name = "broadcaster_id")
     private Integer broadcasterId;
     @Column(name = "broadcaster_name")
     private String broadcasterName;
-
-    @ManyToMany(mappedBy = "blackList")
-//    @JoinTable(name = "users_black_list"
-//            , joinColumns = @JoinColumn(name = "black_list_id")
+    @ManyToMany(mappedBy = "followList")
+//    @JoinTable(name = "users_follow_list"
+//            , joinColumns = @JoinColumn(name = "follow_list_id")
 //            , inverseJoinColumns = @JoinColumn(name = "users_id"))
     private List<User> users;
 }

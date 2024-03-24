5997d9053890a55c64458bbfc5a763aae4961456
AlexLatkin <alexlatkin3@gmail.com>
2024-03-24 18:42:21 +0300

    update

--- src/main/java/com/alexlatkin/twitchclipsbot/telegramBotCommands/buttonCommands/ButtonCommands.java
+++ src/main/java/com/alexlatkin/twitchclipsbot/telegramBotCommands/buttonCommands/ButtonCommands.java
@@ -1,11 +1,11 @@
 package com.alexlatkin.twitchclipsbot.telegramBotCommands.buttonCommands;
 
+import com.alexlatkin.twitchclipsbot.model.entity.Broadcaster;
 import org.telegram.telegrambots.meta.api.methods.updatingmessages.EditMessageText;
 import org.telegram.telegrambots.meta.api.objects.Update;
 import org.telegram.telegrambots.meta.api.objects.replykeyboard.InlineKeyboardMarkup;
 
 public interface ButtonCommands {
     String actionButtonInCurrentMessage(Update update);
-
     InlineKeyboardMarkup keyboard();
 }
--- src/main/java/com/alexlatkin/twitchclipsbot/telegramBotCommands/textCommands/FollowListCommand.java
+++ src/main/java/com/alexlatkin/twitchclipsbot/telegramBotCommands/textCommands/FollowListCommand.java
@@ -1,11 +1,12 @@
 package com.alexlatkin.twitchclipsbot.telegramBotCommands.textCommands;
 
-import com.alexlatkin.twitchclipsbot.controller.BroadcasterController;
 import com.alexlatkin.twitchclipsbot.controller.UserController;
+import com.alexlatkin.twitchclipsbot.model.entity.Broadcaster;
 import lombok.AllArgsConstructor;
 import org.telegram.telegrambots.meta.api.methods.BotApiMethod;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.api.objects.Update;
+
 
 @AllArgsConstructor
 public class FollowListCommand implements BotCommands {
@@ -13,10 +14,20 @@ public class FollowListCommand implements BotCommands {
     @Override
     public BotApiMethod firstMessage(Update update) {
         var chatId = update.getMessage().getChatId();
-        var userFollowList = userController.getUserFollowListByUserChatId(chatId).toString();
+        var userFollowList = userController.getUserFollowListByUserChatId(chatId);
+
+        var bcNames = userFollowList.stream()
+                    .map(Broadcaster::getBroadcasterName)
+                    .map(e -> e.concat("\n"))
+                    .toList()
+                    .toString()
+                    .replace("[","")
+                    .replace("]","")
+                    .replace(",","");
+
         var chatIdString = chatId.toString();
 
-        return new SendMessage(chatIdString, userFollowList);
+        return new SendMessage(chatIdString, bcNames);
     }
 
     @Override
